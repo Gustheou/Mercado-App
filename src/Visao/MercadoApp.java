25007e9890024d87f483bfdc17d01cb9012f4f15
Gustavo <gustavopn117@gmail.com>
2022-05-27 12:32:53 -0300

    Exclusão dos arquivos .class
    Adicionei no gitIgnore para não "upar" os .class

--- src/Visao/MercadoApp.java
+++ src/Visao/MercadoApp.java
@@ -2,7 +2,6 @@ package Visao;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
