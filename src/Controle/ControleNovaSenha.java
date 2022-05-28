e15527ac02cc8c2e87279824c0d96d448b5de362
Gustavo <gustavopn117@gmail.com>
2022-05-27 21:20:20 -0300

    Update ControleNovaSenha.java

--- src/Controle/ControleNovaSenha.java
+++ src/Controle/ControleNovaSenha.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -13,48 +14,55 @@ public class ControleNovaSenha {
 
     @FXML
     private Button finalizarNewSenha_Button;
-
     @FXML
     private Button prosseguirButton;
+    @FXML
+    private Button proseguirButton;
+    @FXML
+    private Button concluirButton;
 
     @FXML
     private TextField novaSenhaCPFTextField;
-
     @FXML
     private TextField novaSenhaUsuarioTextField;
-
     @FXML
     private AnchorPane posProseguirSenha;
-
-    @FXML
-    private Button proseguirButton;
-
-    @FXML
-    private Button concluirButton;
 
     @FXML
     private ImageView imageConcluirButton;
 
     @FXML
-    private VBox userCPF_Vboc;
+    private VBox vboxDados;
+    @FXML
+    private VBox vboxNewSenha;
 
     @FXML
-    void voltarButton(ActionEvent event) {
+    public void voltarButton(ActionEvent event) {
       MercadoApp.changeScreenTelaLogin(event);
     }
 
     @FXML
-    void voltarImageButton(MouseEvent event) {
+    public void voltarImageButton(MouseEvent event) {
       MercadoApp.changeScreenTelaLogin(event);
     }
 
     @FXML
-    void prosseguirButton(ActionEvent event) {
+    public void concluirButton (ActionEvent event) {
+      //Fazer condicional se as senhas sao iguais e depois salvar a nova senha
+    }
+
+    @FXML
+    public void concluirImageButton (ActionEvent event) {
+      //Fazer condicional se as senhas sao iguais e depois salvar a nova senha
+    }
+
+    @FXML
+    public void prosseguirButton(ActionEvent event) {
       //pesquisar nos arquivos
-      novaSenhaCPFTextField.setVisible(false);
-      novaSenhaUsuarioTextField.setVisible(false);
-      prosseguirButton.setVisible(false);
+      vboxDados.setVisible(false);
+      vboxNewSenha.setVisible(true);
+      concluirButton.setVisible(true);
+      imageConcluirButton.setVisible(true);
 
     }
-
 }
